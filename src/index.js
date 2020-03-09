8081d550cdbc4b4796cbab784cd75cd837df0a40
anowshad <al-amin.nowshad@denkwerk.com>
2020-03-09 16:00:10 +0100

    Add a new engpoint to filter by country/parent.

--- src/index.js
+++ src/index.js
@@ -13,5 +13,12 @@ app.get("/api/corona", async (req, res) => {
   res.status(200).json(JSON.parse(rawData));
 });
 
+app.get("/api/corona/:country", async (req, res) => {
+  const rawData = await redisClient.getAsync("cache");
+  const parsedData = JSON.parse(rawData);
+  const result = parsedData.filter(data => data.parent === req.params.country);
+  res.status(200).json(result);
+});
+
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Corona app listening on port ${port}!`));
